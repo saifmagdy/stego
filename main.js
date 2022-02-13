7d21698bdfcfc98f1e28ef97f548734713e0c26e
saif magdy <saifmagdy2720@gmail.com>
2022-02-13 21:13:35 +0200

    new html and css

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function hide() {
+    var x = document.getElementById("hiderow2");
+    var y = document.getElementById("hiderow");
+      x.style.display = "none";
+      y.style.display = "block";
+    }
+
+  function unhide() {
+    var x = document.getElementById("hiderow");
+    var y = document.getElementById("hiderow2");
+      x.style.display = "none";
+      y.style.display = "block";
+  }
